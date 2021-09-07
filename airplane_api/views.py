93e7b61fad502bca82c556b97c849a98477865c4
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-09-07 03:05:41 +0300

    finish

--- airplane_api/views.py
+++ airplane_api/views.py
@@ -2,6 +2,7 @@
 from rest_framework import generics
 from .serializers import AirplanesSerializer
 from .models import Airplane
+from .permissions import IsOwnerOrReadOnly
 # Create your views here.
 
 class AirplanesList(generics.ListCreateAPIView):
@@ -9,5 +10,6 @@ class AirplanesList(generics.ListCreateAPIView):
     serializer_class = AirplanesSerializer
 
 class AirplanesDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Airplane.objects.all()
     serializer_class = AirplanesSerializer
--- airplane_api_project/urls.py
+++ airplane_api_project/urls.py
@@ -18,4 +18,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/airplane/', include('airplane_api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
